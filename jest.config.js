082aa708bd11f60c005d8626b564e58368bdb265
Manz2 <erikmanzer21@gmail.com>
2025-10-24 17:52:22 +0200

    chore: update CI/CD workflows and configuration files

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,24 @@
-export const transform = {
-    '^.+\\.(ts|tsx)$': 'babel-jest',
-};
-export const testEnvironment = 'jsdom';
-export const moduleFileExtensions = ['ts', 'tsx', 'js', 'jsx'];
-export const setupFilesAfterEnv = ['<rootDir>/jest.setup.ts'];
-export const collectCoverage = true;
-export const collectCoverageFrom = ['src/components/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}'];
-export const coverageThreshold = {
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "tsconfig.app.json"
+      },
+    ],
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/components/**/*.{ts,tsx}", "!src/**/*.test.{ts,tsx}"],
+  coverageThreshold: {
     global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
+  },
 };
